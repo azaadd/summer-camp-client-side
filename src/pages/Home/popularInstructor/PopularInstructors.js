8ad40cfdc5f5ca832e930ffdb7190d874c920613
azaadd <abulkalam.kta@gmail.com>
2023-07-28 18:07:41 +0600

    added popular instructors json file

---
+++ src/pages/Home/popularInstructor/PopularInstructors.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import Instructors from './Instructors';
+
+const PopularInstructors = () => {
+
+    
+
+    return (
+        <div className='mx-16 mb-12 mt-16'>
+            
+        </div>
+    );
+};
+
+export default PopularInstructors;
