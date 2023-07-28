ec807f651d74b42166ac834f0f503569823e06a3
azaadd <abulkalam.kta@gmail.com>
2023-07-28 20:22:24 +0600

    added instructor page

--- src/pages/instructors/Instructors.js
+++ src/pages/instructors/Instructors.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import TalentInstructor from './TalentInstructor';
 
 const Instructors = () => {
+    const [pageInstructor, setPageInstructor] = useState([]);
+
+    useEffect(() => {
+        fetch('instructors.json')
+        .then(res => res.json())
+        .then(data => setPageInstructor(data))
+    },[]);
+
+
     return (
-        <div>
+        <div className='pt-36 mb-12'>
             <Helmet>
                 <title>Language School | Instructors</title>
             </Helmet>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+                {
+                    pageInstructor.map(instructor => <TalentInstructor
+                    key={instructor._id}
+                    instructor={instructor}
+                    ></TalentInstructor>)
+                }
+            </div>
             
         </div>
     );
---
+++ src/pages/instructors/TalentInstructor.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TalentInstructor = ({instructor}) => {
+    const {photoUrl, name, Lecturer, totalEnrolled, email} = instructor;
+
+    return (
+        <div className="card w-full bg-base-100 shadow-xl">
+            <figure className="px-3 pt-3">
+                <img src={photoUrl} alt="Shoes" className="rounded-xl w-[372px] h-[248px]" />
+            </figure>
+            <div className="card-body items-center text-center mt-0 pt-2 pb-5">
+                <h2 className="card-title text-2xl">{name}</h2>
+                <p>Lecturer: {Lecturer}</p>
+                <p>Total Student: {totalEnrolled}</p>
+                <p className='font-semibold'>Email: {email}</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary normal-case text-lg md:w-80">Book your appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TalentInstructor;
