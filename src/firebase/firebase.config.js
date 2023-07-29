e2f18b88574db75102110d8da4cb9ac5c96ef4ec
azaadd <abulkalam.kta@gmail.com>
2023-07-29 17:43:00 +0600

    added .env.local file and its config in client side

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5-GrLq_COrpLTn3LttC6VcHPQM4fL0os",
-  authDomain: "language-school-1be90.firebaseapp.com",
-  projectId: "language-school-1be90",
-  storageBucket: "language-school-1be90.appspot.com",
-  messagingSenderId: "549843443585",
-  appId: "1:549843443585:web:07e0c2a0f7751f1792d280"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
