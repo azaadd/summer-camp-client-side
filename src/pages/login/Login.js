5950a539370661530e19824024bcd2d9ce1b7645
azaadd <abulkalam.kta@gmail.com>
2023-07-30 02:27:59 +0600

    added show password field in login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import googlelogo from '../../../src/assets/google.png';
 import { AuthContext } from '../../providers/AuthProvider';
 
 const Login = () => {
-    const {signIn, signInWithGoogle} = useContext(AuthContext);
+    const [isShown, setIsSHown] = useState(false);
+    const { signIn, signInWithGoogle } = useContext(AuthContext);
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -15,33 +16,38 @@ const Login = () => {
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        
+
         console.log(email, password)
 
         signIn(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-            form.reset();
-            navigate(from, {replace: true});
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                navigate(from, { replace: true });
+            })
+            .catch(error => {
+                console.log(error)
+            })
 
     }
 
     const handleGoogleSignIn = () => {
         signInWithGoogle()
-        .then(result => {
-            const loggedUser = result.user;
-            console.log(loggedUser);
-            navigate(from, {replace: true});
-        })
-        .catch(error => {
-            console.log(error);
-        })
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate(from, { replace: true });
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+
+    const togglePassword = () => {
+        setIsSHown((isShown) => !isShown);
+    };
+
 
     return (
         <div className="hero bg-base-200 w-full pt-28">
@@ -62,19 +68,28 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text font-semibold text-lg">Password</span>
                                 </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <input type={isShown ? "text" : "password"} name='password' id='myInput' placeholder="password" className="input input-bordered" required />
 
+                            </div>
+                            <div className="checkbox-container mt-3">
+                                <input className='w-5 h-5 mr-2'
+                                    id="checkbox"
+                                    type="checkbox"
+                                    checked={isShown}
+                                    onChange={togglePassword}
+                                />
+                                <label className='text-lg'  htmlFor="checkbox">Show password?</label>
                             </div>
                             <div className="form-control mt-6">
                                 <input className="btn btn-primary font-semibold hover:bg-opacity-75 normal-case text-lg" type="submit" value="Login" />
                             </div>
                             <p className='mt-3'>Don't have any account? <Link className='text-blue-600 font-semibold underline hover:text-gray-600' to='/register'>Register.</Link></p>
                         </form>
-                        
+
                     </div>
-                    
+
                 </div>
-                
+
                 <Link onClick={handleGoogleSignIn} className="btn btn-outline border-2 normal-case text-xl text-[#0d6efd] hover:bg-[#0d6efd] w-full mb-6" type="submit"> <img className='w-7 h-7' src={googlelogo} alt="" /> Sign-in With Google</Link>
             </div>
 
